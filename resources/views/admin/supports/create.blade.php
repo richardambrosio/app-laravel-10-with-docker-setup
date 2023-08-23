85fbfec6e76008cc0bacad365367296a8885a1de
Richard Ambrosio <richard.ambrosio1@live.com>
2023-08-23 15:18:55 -0300

    organizing the views

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,14 +1,7 @@
 <h1>Nova Dúvida</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}<br>
-    @endforeach
-@endif
+<x-alert></x-alert>
 
 <form action="{{ route('supports.store') }}" method="post">
-    @csrf
-    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
-    <textarea name="body" cols="30" rows="3" placeholder="Descrição">{{ old('body') }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form')
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,15 +1,11 @@
 <h1>Dúvida {{ $support->id }}</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}<br>
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.update', $support->id) }}" method="post">
     @csrf
     @method('put')
-    <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="3" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
