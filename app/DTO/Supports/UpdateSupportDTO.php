5756c71065e91e8c70f6d547c985ab8d2fc6ce61
Richard Ambrosio <richard.ambrosio1@live.com>
2023-08-29 11:27:41 -0300

    API Rest using the same Service anda Adapter in the index()

--- app/DTO/Supports/UpdateSupportDTO.php
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -14,10 +14,10 @@ class UpdateSupportDTO
         public string $body
     ) {}
 
-    public static function makeFromRequest(StoreUpdateSupportRequest $request): self
+    public static function makeFromRequest(StoreUpdateSupportRequest $request, string $id = null): self
     {
         return new self(
-            $request->id,
+            $id ?? $request->id,
             $request->subject,
             SupportStatusEnum::A,
             $request->body
